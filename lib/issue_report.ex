5e5e3f00a460fc8df5a5e531dcf43f0cdc00b296
Gabriel Vargas <assuncao.gabriel12@gmail.com>
2019-10-09 12:57:13 -0300

    Init project

---
+++ lib/issue_report.ex
@@ -0,0 +1,18 @@
+defmodule IssueReport do
+  @moduledoc """
+  Documentation for IssueReport.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> IssueReport.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/issue_report_test.exs
@@ -0,0 +1,8 @@
+defmodule IssueReportTest do
+  use ExUnit.Case
+  doctest IssueReport
+
+  test "greets the world" do
+    assert IssueReport.hello() == :world
+  end
+end
